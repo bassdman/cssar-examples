3924882c01204954b3f834171213860f7a1be192
Manuel Gelsen <manuel.gelsen@gmail.com>
2019-05-24 07:40:17 +0200

    now css by default

--- 02_run_in_node/cssar-config.js
+++ 02_run_in_node/cssar-config.js
@@ -3,5 +3,5 @@ module.exports = {
         src and dest are required if only one parameter is passed.
     */
     src: './template.html',
-    dest: 'dist/output.html',
+    dest: 'dist/output.css',
 }
